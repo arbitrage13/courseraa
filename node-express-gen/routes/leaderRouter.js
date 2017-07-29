ed219f159f54aa0d8c9f3800759cc98fada7a3a5
SURAT-PC\SURAT <arbitrage13@gmail.com>
2017-07-30 00:19:01 +0700

    node-express-gen

---
+++ node-express-gen/routes/leaderRouter.js
@@ -0,0 +1,47 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+var leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+})
+
+.get(function(req,res,next){
+        res.end('Will send all the leader to you!');
+})
+
+.post(function(req, res, next){
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);    
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting all leaders');
+});
+
+leaderRouter.route('/:leadershipId')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+})
+
+.get(function(req,res,next){
+        res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+})
+
+.put(function(req, res, next){
+        res.write('Updating the leader: ' + req.params.leadershipId + '\n');
+    res.end('Will update the leader: ' + req.body.name + 
+            ' with details: ' + req.body.description);
+})
+
+.delete(function(req, res, next){
+        res.end('Deleting leaders: ' + req.params.leaderId);
+})
+
+module.exports = leaderRouter;
+
