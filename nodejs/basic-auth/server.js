c0ca45e0990b53b70447f6e852b1f42415eb9115
arbitrage13 <arbitrage13@gmail.com>
2017-08-24 17:14:00 +0700

    no message

---
+++ nodejs/basic-auth/server.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+
+function auth (req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+        return;
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(__dirname + '/public'));
+app.use(function(err,req,res,next) {
+            res.writeHead(err.status || 500, {
+            'WWW-Authenticate': 'Basic',
+            'Content-Type': 'text/plain'
+        });
+        res.end(err.message);
+});
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+/*var express = require('express');
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var app = express();
+
+app.use(morgan('dev'));
+//////////////Basic Authentications//////////////////////////
+function auth (req, res, next){
+    console.log(req.headers);
+    //expect the username&password from client side
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+        var err = new Error('You are not authenticated');
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer(authHeader.split('')[1],'base64').toString().split(':'); // base65 encoding
+    var user = auth[0];
+    var pass = auth[1];
+    //check user//
+    if (user == 'admin' && pass == 'password'){
+        next(); // authorized
+    }
+    else{
+        var err = new Error('You are not authenticated');
+        err.status = 401;
+        next(err);
+    }
+}
+
+app.use(auth); // use middleware function
+/////////////////////////////////////////
+
+app.use(express.static(__dirname + '/public'));
+///handler_ERR////
+app.use(function(err,req,res,next){
+    res.writeHead(err.status || 500, {
+        'WWW-Authenticate': 'Basic',
+        'Content-Type': 'text/plain'
+    });
+    res.end(err.message);
+});
+
+app.listen(port, hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
